de5f1ba4091f84ec666657d8d02ba43c72a21cd1
Yuta Tokoi <yutatokoi@proton.me>
2025-02-28 08:49:41 +0900

    update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,9 +1,10 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_URL = "https://miniblog.nicholasly.com";
-export const SITE_TITLE = "Miniblog";
+export const SITE_URL = "https://yutatokoi.com";
+export const SITE_TITLE = "Yuta Tokoi";
 export const SITE_DESCRIPTION =
-  "Miniblog is an opinionated and extremely minimal blogging template built with Astro and Tailwind CSS.";
+  "Yuta Tokoi's Website | 床井祐太のウェブサイト";
 
-export const EMAIL = "hello@nicholasly.com";
+export const EMAIL = "yutatokoi@proton.me";
+
