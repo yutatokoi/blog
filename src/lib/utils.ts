69b4305256ed98e0f4d7a22208f0babbe21fc484
Yuta Tokoi <yutatokoi@proton.me>
2025-02-28 13:54:01 +0900

    change date to ISO8601

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,10 +5,11 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+// https://stackoverflow.com/questions/25050034/get-iso-8601-using-intl-datetimeformat
 export function formatDate(date: Date) {
-  return Intl.DateTimeFormat("en-US", {
+  return Intl.DateTimeFormat("sv-SE", {
     year: "numeric",
-    month: "long",
-    day: "numeric",
+    month: "2-digit",
+    day: "2-digit",
   }).format(date);
 }
