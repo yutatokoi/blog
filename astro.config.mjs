fb0be90c5a5da45b59e056962722b0d4fa378b2d
Yuta Tokoi <yutatokoi@proton.me>
2026-03-15 17:35:25 +1100

    feat: upgrade outdated packages and remove unused blog features

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,16 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
+import tailwindcss from "@tailwindcss/vite";
 
 import sitemap from "@astrojs/sitemap";
 
-import tailwind from "@astrojs/tailwind";
 import { SITE_URL } from "./src/consts";
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE_URL,
-  integrations: [mdx(), sitemap(), tailwind()],
-  markdown: {
-    shikiConfig: {
-      themes: {
-        light: "catppuccin-latte",
-        dark: "catppuccin-mocha",
-      },
-    },
+  integrations: [sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
