ccd8b4ad4977142b971d80e06b95af41c2a8eca9
Yuta Tokoi <yutatokoi@proton.me>
2026-03-15 15:06:31 +1100

    feat: consolidate content to single page. Take inspiration from simplicity of https://katei.dev/

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,14 +4,6 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   darkMode: "class",
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Geist", ...defaultTheme.fontFamily.sans],
-        mono: ["'Geist Mono'", ...defaultTheme.fontFamily.mono],
-      },
-    },
-  },
   plugins: [],
 };
 
